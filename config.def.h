4b6c5a4d8f568e73a86dfc389c164713e0532b7d
Jamie Frost <jamcris11@hotmail.co.uk>
2025-07-26 15:32:02 +0100

    New: add script cmds

--- config.def.h
+++ config.def.h
@@ -67,8 +67,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *nscmd[] = { "ns-dmenu", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *pywalcmd[] = { "pywal-wallpaper", NULL };
 
 /*
  * Xresources preferences to load at startup
@@ -95,7 +97,9 @@ ResourcePref resources[] = {
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,						XK_s,	   spawn,		   {.v = nscmd } },
 	{ MODKEY,			            XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_w,	   spawn,		   {.v = pywalcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
