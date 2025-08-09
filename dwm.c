cb96afd56278cd21ad10b5230a95fc19f517b85b
Jamie Frost <jamcris11@hotmail.co.uk>
2025-08-09 21:50:18 +0100

    Alt: spawn mouse bottom-right on monfocus change

--- dwm.c
+++ dwm.c
@@ -855,7 +855,7 @@ focusmon(const Arg *arg)
 	if ((m = dirtomon(arg->i)) == selmon)
 		return;
 	unfocus(selmon->sel, 0);
-	XWarpPointer(dpy, None, m->barwin, 0, 0, 0, 0, m->mw / 2, m->mh / 2);
+	XWarpPointer(dpy, None, m->barwin, 0, 0, 0, 0, m->mw-1, m->mh-1);
 	selmon = m;
 	focus(NULL);
 }
