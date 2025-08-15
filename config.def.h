518fd6af090c07dddf4381be5d6a667e3ebc94a9
jamcris11 <jamcris11@hotmail.co.uk>
2025-08-15 23:19:06 +0100

    New: chromium icon

--- config.def.h
+++ config.def.h
@@ -74,6 +74,7 @@ static const Rule rules[] = {
 	{ NULL,		 "spterm", NULL,		   SPTAG(0),  1,		  1,		   1,        -1,	NULL },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1,	NULL }, /* xev */
 	{ "steam",   NULL,     NULL,		   0,         0,          0,           1,        -1,	"󰓓" }, 
+	{ "Chromium",  NULL,     NULL,		   1 << 7,    0,          0,           1,        -1,	"" }, 
 };
 
 /* layout(s) */
